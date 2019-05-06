992e984ad1d9940513101b1394a237dc41ee9b0a
br1code <brunogiovagnoli@gmail.com>
2019-05-06 12:39:56 -0300

    remove patient argument

--- medicalRecordsContract.sol
+++ medicalRecordsContract.sol
@@ -1,50 +1,45 @@
 pragma solidity ^0.5.0;
 
 contract MedicalRecords {
-    
+
     struct MedicalRecord {
         uint age;
         string name;
         string bloodGroup;
         bool exists;
     }
-    
+
     mapping(address => MedicalRecord) medicalRecords;
-    
+
     mapping(address => string) accessRecords;
-    
+
     event onMedicalRecordRequested(uint age, string name, string bloodGroup);
-    
-    function upsertMedicalRecord(address patient, uint age, string memory name, string memory bloodGroup) public {
-        require(onlyMedicalRecordsOwner(patient));
-        
-        MedicalRecord storage medicalRecord = medicalRecords[patient];
-        
+
+    function upsertMedicalRecord(uint age, string memory name, string memory bloodGroup) public {
+
+        MedicalRecord storage medicalRecord = medicalRecords[msg.sender];
+
         medicalRecord.age = age;
         medicalRecord.name = name;
         medicalRecord.bloodGroup = bloodGroup;
         medicalRecord.exists = true;
     }
-    
+
     function requestMedicalRecord(address patient, string memory name, string memory timeStamp) public {
         MedicalRecord storage medicalRecord = medicalRecords[patient];
-        
+
         if (medicalRecord.exists) {
             string memory prevAccessRecord = accessRecords[patient];
             string memory newAccessRecord = string(abi.encodePacked(prevAccessRecord, timeStamp, ' ', name, ' - '));
-             
+
             accessRecords[patient] = newAccessRecord;
-            
+
             emit onMedicalRecordRequested(medicalRecord.age, medicalRecord.name, medicalRecord.bloodGroup);
         }
     }
-    
-    function getAccessRecords(address patient) view public returns (string memory) {
-        require(onlyMedicalRecordsOwner(patient));
-        return accessRecords[patient];
+
+    function getAccessRecords() public view returns (string memory) {
+        return accessRecords[msg.sender];
     }
-    
-    function onlyMedicalRecordsOwner(address patient) view private returns (bool) {
-        return msg.sender == patient;
-    }
+
 }
